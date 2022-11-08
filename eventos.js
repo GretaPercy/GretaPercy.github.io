5ee500cecec856e2a28fb3e51c3f98b634767442
Percy Villajulca <percyvillajulcar@gmail.com>
2022-11-08 11:00:16 -0500

    boton mas visible

--- eventos.js
+++ eventos.js
@@ -6,11 +6,15 @@ function colocar(event){
 	let map1 = document.querySelector("div#mapa1");
 	let map2 = document.querySelector("div#mapa2");
 	
+	let alto = ivt.height*0.03; 
+	
 	map1.style.top = ivt.height/2 + ivt.height*0.1;
 	map1.style.left = ivt.width/2 + ivt.height*0.07;
-	map1.style.width = ivt.height*0.035;
+	map1.style.width = alto;
+	map1.style.height = alto;
 	
 	map2.style.top = ivt.height/2 + ivt.height*0.2;
 	map2.style.left = ivt.width/2 + ivt.height*0.07;
-	map2.style.width = ivt.height*0.035;
+	map2.style.width = alto;
+	map2.style.height = alto;
 }
