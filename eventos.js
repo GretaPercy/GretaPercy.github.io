3c3f3b750911a9d0a0f6939233bc2614269f840c
Percy Villajulca <percyvillajulcar@gmail.com>
2022-11-08 11:51:58 -0500

    Ajuste posición

--- eventos.js
+++ eventos.js
@@ -6,15 +6,15 @@ function colocar(event){
 	let map1 = document.querySelector("div#mapa1");
 	let map2 = document.querySelector("div#mapa2");
 	
-	let diam = ivt.height*0.05;
+	let diam = ivt.height*0.04;
 	
-	map1.style.top = ivt.height/2 + diam*0.9;
-	map1.style.left = ivt.width/2 + diam*4.2;
+	map1.style.top = ivt.height/2 + diam*1.2;
+	map1.style.left = ivt.width/2 + diam*5.1;
 	map1.style.width = diam;
 	map1.style.height = diam;
 	
-	map2.style.top = ivt.height/2 + diam*3.5;
-	map2.style.left = ivt.width/2 + diam*4.2;
+	map2.style.top = ivt.height/2 + diam*4.4;
+	map2.style.left = ivt.width/2 + diam*5.1;
 	map2.style.width = diam;
 	map2.style.height = diam;
 }
